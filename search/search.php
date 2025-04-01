e51219cb5c2c3048ee97bdd65c138b148fb3312b
BlueFlame6666 <sh2267@naver.com>
2025-04-01 13:33:18 +0900

    검색결과 및 시뮬레이터 로그 저장 기능 추가
    캐릭터 검색 및 시뮬레이터 접속시,
    사용자의 현재 URL을 DB에 저장

--- search/search.php
+++ search/search.php
@@ -499,6 +499,44 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <?php //  db관련 
     ?>
+    
+    <script type="text/javascript">
+        $(document).ready(function() {
+            insertLopecLog();
+        });
+
+        var insertLopecLog = function() {
+            var atMode = "insertlog";
+            var llogUrl = document.URL;
+            var saveDatas = {
+                atMode: atMode,
+                llogUrl: llogUrl
+            }
+            $.ajax({
+                dataType: "json",
+                type: "POST",
+                url: "/applications/process/lopecLog/",
+                data: saveDatas,
+                success: function(msg) {
+                    // console.log("msg : " + msg);
+                    // console.log("msg.result : " + msg.result);
+                    if (msg.result == "S") {
+                        // console.log("log insert result : LOPEC_LOG 저장 성공");
+                    } else if (msg.result == "F") {
+                        // console.log("log insert result : LOPEC_LOG 저장 실패");
+                    } else if (msg.result == "E") {
+                        // console.log("log insert result : LOPEC_LOG 저장 Exception");
+                    }
+                },
+                error: function(request, status, error) {
+                    // console.log("log insert result : LOPEC_LOG 저장 Error");
+                    // console.log("request.status : " + request.status);
+                    // console.log("request.responseText : " + request.responseText);
+                    // console.log("request.error : " + request.error);
+                }
+            });
+        }
+    </script>
 
 
     <script>
